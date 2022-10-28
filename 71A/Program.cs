5bdf08adb9f62828b0ea295d20b4a4d0a56b5460
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-10-28 08:58:19 +0600

    71A: Accepted

--- 71A/Program.cs
+++ 71A/Program.cs
@@ -1,6 +1,27 @@
 ﻿/**
  * Problem Link - https://codeforces.com/problemset/problem/71/A
  * Author - Ahnaf Shahriar
- * Status - Trying
- * Date & Time - 28 Oct 2022, 08:47 AM
+ * Status - Accepted
+ * Date & Time - 28 Oct 2022, 08:57 AM
  */
+
+int testCase = int.Parse(Console.ReadLine());
+
+while (testCase > 0)
+{
+    string word = Console.ReadLine();
+    int wordLength = word.Length;
+    string output = "";
+    if (wordLength > 10)
+    {
+        int wordLengthOutput = wordLength - 2;
+        output = output + word[0] + wordLengthOutput.ToString() + word[wordLength-1];
+        Console.WriteLine(output);
+    }
+    else
+    {
+        Console.WriteLine(word);
+    }
+
+    testCase--;
+}
