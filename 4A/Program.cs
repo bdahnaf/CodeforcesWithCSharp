43f0cca9c39c60d381e6a9bf5140035593a32382
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-10-28 08:44:05 +0600

    4A: AC

--- 4A/Program.cs
+++ 4A/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**
+ * Problem Link - https://codeforces.com/problemset/problem/4/A
+ * Author - Ahnaf Shahriar
+ * Status - Accepted
+ * Date & Time - 28 Oct 2022, 08:43 AM
+ */
+int w = int.Parse(Console.ReadLine());
+if (w >= 1 && w <= 100)
+{
+    if (w % 2 == 0 && w != 2)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
