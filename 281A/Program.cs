884cbfc91cb23af3c93d8e45ce7db68d151cee4f
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-10-30 22:57:57 +0600

    281A: AC

---
+++ 281A/Program.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * Problem Link - https://codeforces.com/problemset/problem/281/A
+ * Author - Ahnaf Shahriar
+ * Status - Accepted
+ * Date & Time - 30 Oct 2022, 10:57 PM
+ */
+
+string word = Console.ReadLine();
+if ((int)word[0] > 90)
+{
+    int wordInInt = (int)word[0] - 32;
+    Console.Write((char)(wordInInt));
+    for (int i = 1; i < word.Length; i++)
+    {
+        Console.Write(word[i]);
+    }
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine(word);
+}
