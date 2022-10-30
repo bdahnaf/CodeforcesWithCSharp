f90aa4af3b915a2b537470c05389faa5f761d147
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-10-30 11:56:46 +0600

    Add problem statement

--- 158A/Program.cs
+++ 158A/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**
+ * Problem Link - https://codeforces.com/problemset/problem/158/A
+ * Author - Ahnaf Shahriar
+ * Status - Trying
+ * Date & Time - 30 Oct 2022, 10:38 AM
+ */
+
+string firstLine = Console.ReadLine();
+string secondLine = Console.ReadLine();
+
+string[] data = firstLine.Split(' ');
+int n = int.Parse(data[0]);
+int k = int.Parse(data[1]);
+data = secondLine.Split(' ');
+int[] a = new int[n];
+int i = 0;
+foreach (var number in data)
+{
+    a[i] = int.Parse(number);
+    i++;
+}
