5738acc54d8c20bfb640534a9081ccb8d5510eb5
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-10-30 22:20:56 +0600

    112A: AC

--- 112A/Program.cs
+++ 112A/Program.cs
@@ -1,47 +1,43 @@
 ﻿/**
  * Problem Link - https://codeforces.com/problemset/problem/112/A
  * Author - Ahnaf Shahriar
- * Status - Trying
- * Date & Time - 30 Oct 2022, 12:58 PM
+ * Status - Accepted
+ * Date & Time - 30 Oct 2022, 10:20 PM
  */
-/**
- * aslkjlkasdd
- * asdlkjdajwi
-*/
+
 string first = Console.ReadLine();
 string second = Console.ReadLine();
-int len = first.Length;
-int sumF = 0;
-int sumS = 0;
-for (int i = 0; i < len; i++)
+int charValueF = 0;
+int charValueS = 0;
+for (int i = 0; i < first.Length; i++)
 {
     if ((int)first[i] <= 90)
     {
-        sumF = sumF + (int)first[i] + 32;
+        charValueF = (int)first[i] + 32;
     }
     else
     {
-        sumF = sumF + (int)first[i];
+        charValueF = (int)first[i];
     }
     if ((int)second[i] <= 90)
     {
-        sumS = sumS + (int)second[i] + 32;
+        charValueS = (int)second[i] + 32;
     }
     else
     {
-        sumS = sumS + (int)second[i];
+        charValueS = (int)second[i];
+    }
+
+    if (charValueF < charValueS)
+    {
+        Console.WriteLine("-1");
+        return;
+    }
+    if (charValueF > charValueS)
+    {
+        Console.WriteLine("1");
+        return;
     }
 }
 
-if(sumF == sumS)
-{
-    Console.WriteLine("0");
-}
-else if (sumF < sumS)
-{
-    Console.WriteLine("-1");
-}
-else if (sumF > sumS)
-{
-    Console.WriteLine("1");
-}
+Console.WriteLine("0");
