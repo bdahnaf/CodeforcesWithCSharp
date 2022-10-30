a11267ed68f282d3b8964b94b55971e4600b68dd
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-10-30 23:38:29 +0600

    118A: AC

--- 118A/Program.cs
+++ 118A/Program.cs
@@ -4,3 +4,27 @@
  * Status - Accepted
  * Date & Time - 30 Oct 2022, 11:29 PM
  */
+string word = Console.ReadLine();
+string output = "";
+
+for (int i = 0; i < word.Length; i++)
+{
+
+    if (word[i] == 'A' || word[i] == 'O' || word[i] == 'Y' || word[i] == 'E' || word[i] == 'U' || word[i] == 'I')
+    {
+        output = output + "";
+    }
+    else if (word[i] == 'a' || word[i] == 'o' || word[i] == 'y' || word[i] == 'e' || word[i] == 'u' || word[i] == 'i')
+    {
+        output = output + "";
+    }
+    else if ((int)word[i] <= 90)
+    {
+        output = output + "." + (char)((int)word[i] + 32);
+    }
+    else
+    {
+        output = output + "." + word[i];
+    }
+}
+Console.WriteLine(output);
