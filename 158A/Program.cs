857852a7f291e08c155fa7480fbe42992d05a2b7
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-10-30 12:16:56 +0600

    158A: AC

--- 158A/Program.cs
+++ 158A/Program.cs
@@ -1,8 +1,8 @@
 ﻿/**
  * Problem Link - https://codeforces.com/problemset/problem/158/A
  * Author - Ahnaf Shahriar
- * Status - Trying
- * Date & Time - 30 Oct 2022, 10:38 AM
+ * Status - Accepted
+ * Date & Time - 30 Oct 2022, 12:16 PM
  */
 
 string firstLine = Console.ReadLine();
@@ -14,8 +14,20 @@ int k = int.Parse(data[1]);
 data = secondLine.Split(' ');
 int[] a = new int[n];
 int i = 0;
+
 foreach (var number in data)
 {
     a[i] = int.Parse(number);
     i++;
 }
+
+int value = a[k-1];
+int result = 0;
+for(int j = 0; j < n; j++)
+{
+    if (a[j] >= value && a[j] > 0)
+    {
+        result++;
+    }
+}
+Console.WriteLine(result);
