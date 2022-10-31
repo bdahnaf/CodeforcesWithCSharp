f49242f7e7a6725a7484abed6893682ad8e95dc0
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-10-31 15:46:37 +0600

    791A: AC

--- 791A/Program.cs
+++ 791A/Program.cs
@@ -1,6 +1,19 @@
 ﻿/**
  * Problem Link - https://codeforces.com/problemset/problem/791/A
  * Author - Ahnaf Shahriar
- * Status - Trying
- * Date & Time - 30 Oct 2022, 03:36 PM
+ * Status - Accepted
+ * Date & Time - 30 Oct 2022, 03:46 PM
  */
+string input = Console.ReadLine();
+int[] inputArray = Array.ConvertAll(input.Split(' '), int.Parse);
+int a = inputArray[0];
+int b = inputArray[1];
+int y = 0;
+
+while(a <= b)
+{
+    y++;
+    a *= 3;
+    b *= 2;
+}
+Console.WriteLine(y);
