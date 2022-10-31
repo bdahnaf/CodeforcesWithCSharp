6f3f89433e115ae62dc4e5165afb8ec9f7b619a0
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-10-31 15:29:10 +0600

    546A: AC

--- 546A/Program.cs
+++ 546A/Program.cs
@@ -1,6 +1,26 @@
 ﻿/**
  * Problem Link - https://codeforces.com/problemset/problem/546/A
  * Author - Ahnaf Shahriar
- * Status - Trying
- * Date & Time - 31 Oct 2022, 3:11 PM
+ * Status - Accepted
+ * Date & Time - 31 Oct 2022, 3:29 PM
  */
+
+string input = Console.ReadLine();
+int[] inputArray = Array.ConvertAll(input.Split(' '), int.Parse);
+int k = inputArray[0];
+int n = inputArray[1];
+int w = inputArray[2];
+int sum = 0;
+
+for(int i = 1; i <= w; i++)
+{
+    sum += i * k;
+}
+if(sum >= n)
+{
+    Console.WriteLine(sum - n);
+}
+else
+{
+    Console.WriteLine("0");
+}
