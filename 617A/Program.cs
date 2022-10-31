1918cca780c56ae92f23a3796ed168e0571d0547
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-10-31 20:24:19 +0600

    Add new problem: 617A

--- 617A/Program.cs
+++ 617A/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**
+ * Problem Link - https://codeforces.com/problemset/problem/617/A
+ * Author - Ahnaf Shahriar
+ * Status - Accepted
+ * Date & Time - 31 Oct 2022, 08:23 PM
+ */
+
+int number = int.Parse(Console.ReadLine());
