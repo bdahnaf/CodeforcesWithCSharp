ed5b92299e64fed0a6cfa64dbac32abe3bfd6a85
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-10-31 20:30:01 +0600

    617A: AC

--- 617A/Program.cs
+++ 617A/Program.cs
@@ -2,7 +2,33 @@
  * Problem Link - https://codeforces.com/problemset/problem/617/A
  * Author - Ahnaf Shahriar
  * Status - Accepted
- * Date & Time - 31 Oct 2022, 08:23 PM
+ * Date & Time - 31 Oct 2022, 08:29 PM
  */
 
 int number = int.Parse(Console.ReadLine());
+int result = 0;
+while (number > 0)
+{
+    if (number >= 5)
+    {
+        number -= 5;
+    }
+    else if (number >= 4)
+    {
+        number -= 4;
+    }
+    else if (number >= 3)
+    {
+        number -= 3;
+    }
+    else if (number >= 2)
+    {
+        number -= 2;
+    }
+    else if (number >= 1)
+    {
+        number -= 1;
+    }
+    result++;
+}
+Console.WriteLine(result);
