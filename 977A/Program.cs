767d8bd3142f7fa8a021f0a1c50f1f0862f1825c
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-02 20:47:56 +0600

    977A: AC

--- 977A/Program.cs
+++ 977A/Program.cs
@@ -1,7 +1,27 @@
 ﻿/**
  * Problem Link - https://codeforces.com/problemset/problem/977/A
  * Author - Ahnaf Shahriar
- * Status - Trying
- * Date & Time - 02 Nov 2022, 08:40 PM
+ * Status - Accepted
+ * Date & Time - 02 Nov 2022, 08:47 PM
  */
 
+string input = Console.ReadLine();
+int[] inputInt = Array.ConvertAll(input.Split(' '), int.Parse);
+int n = inputInt[0];
+int k = inputInt[1];
+int lastDigit = 0;
+while (k > 0)
+{
+    lastDigit = n % 10;
+    if (lastDigit == 0)
+    {
+        n = n / 10;
+    }
+    else
+    {
+        n -= 1;
+    }
+
+    k--;
+}
+Console.WriteLine(n);
