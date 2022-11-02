25b8c426e4150c682d93133dfbec9b8942a913d5
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-02 20:55:25 +0600

    Add new problem: 110A

---
+++ 110A/Program.cs
@@ -0,0 +1,9 @@
+﻿/**
+ * Problem Link - https://codeforces.com/problemset/problem/110/A
+ * Author - Ahnaf Shahriar
+ * Status - Trying
+ * Date & Time - 02 Nov 2022, 08:52 PM
+ */
+
+ulong number = UInt64.Parse(Console.ReadLine());
+Console.WriteLine(number);
