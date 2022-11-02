62c5f66cf15979e30e6b7a31fd0dc04d5ddecd29
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-02 23:34:07 +0600

    116A: AC

---
+++ 116A/Program.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * Problem Link - https://codeforces.com/problemset/problem/116/A
+ * Author - Ahnaf Shahriar
+ * Status - Accepted
+ * Date & Time - 31 Oct 2022, 11:33 PM
+ */
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+int a = 0;
+int b = 0;
+int ans = 0;
+while (n > 0)
+{
+    int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+    a = input[0];
+    b = input[1];
+    sum = sum + b - a;
+    if (sum >= ans)
+    {
+        ans = sum;
+    }
+    n--;
+}
+Console.WriteLine(ans);
