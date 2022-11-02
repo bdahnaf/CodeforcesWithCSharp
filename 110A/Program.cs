74f1e839af52f7137fb1f7d36ac0cbab29b5c306
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-02 23:17:37 +0600

    110A: AC

--- 110A/Program.cs
+++ 110A/Program.cs
@@ -2,8 +2,44 @@
  * Problem Link - https://codeforces.com/problemset/problem/110/A
  * Author - Ahnaf Shahriar
  * Status - Trying
- * Date & Time - 02 Nov 2022, 08:52 PM
+ * Date & Time - 02 Nov 2022, 11:17 PM
  */
 
 ulong number = UInt64.Parse(Console.ReadLine());
-Console.WriteLine(number);
+ulong lastDigit = 0;
+ulong lucky = 0;
+int flag = 0;
+
+while (number > 0)
+{
+    lastDigit = number % 10;
+    if (lastDigit == 4 || lastDigit == 7)
+    {
+        lucky++;
+    }
+
+    number /= 10;
+}
+while (lucky > 0)
+{
+    lastDigit = lucky % 10;
+    if (lastDigit == 4 || lastDigit == 7)
+    {
+        flag = 1;
+    }
+    else
+    {
+        flag = 0;
+        break;
+    }
+    lucky /= 10;
+}
+
+if (flag == 1)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
