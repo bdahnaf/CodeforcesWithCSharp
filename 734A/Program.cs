85fee6d4bd304338cb9b500514aa66e660b2d1ab
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-02 23:41:43 +0600

    734A: AC

--- 734A/Program.cs
+++ 734A/Program.cs
@@ -1,6 +1,34 @@
 ﻿/**
  * Problem Link - https://codeforces.com/problemset/problem/734/A
  * Author - Ahnaf Shahriar
- * Status - Trying
- * Date & Time - 02 Nov 2022, 11:36 PM
+ * Status - Accepted
+ * Date & Time - 02 Nov 2022, 11:41 PM
  */
+int n = int.Parse(Console.ReadLine());
+string input = Console.ReadLine();
+int a = 0;
+int b = 0;
+for (int i = 0; i < n; i++)
+{
+    if (input[i] == 'A')
+    {
+        a++;
+    }
+    else
+    {
+        b++;
+    }
+}
+
+if (a == b)
+{
+    Console.WriteLine("Friendship");
+}
+else if (a > b)
+{
+    Console.WriteLine("Anton");
+}
+else
+{
+    Console.WriteLine("Danik");
+}
