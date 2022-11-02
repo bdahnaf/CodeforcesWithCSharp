7d736e8143de88800b84db8d29d20f911f2eee29
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-02 20:41:04 +0600

    Add comments

--- 977A/Program.cs
+++ 977A/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**
+ * Problem Link - https://codeforces.com/problemset/problem/977/A
+ * Author - Ahnaf Shahriar
+ * Status - Trying
+ * Date & Time - 02 Nov 2022, 08:40 PM
+ */
+
