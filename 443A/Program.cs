4c63dc7a33d88319648e7371abcbb4a0a0a03a7a
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-03 23:05:38 +0600

    434A: AC

--- 443A/Program.cs
+++ 443A/Program.cs
@@ -2,7 +2,7 @@
  * Problem Link - https://codeforces.com/problemset/problem/443/A
  * Author - Ahnaf Shahriar
  * Status - Trying
- * Date & Time - 30 Oct 2022, 03:35 PM
+ * Date & Time - 03 November 2022, 03:35 PM
  */
 
 string line = Console.ReadLine();
@@ -12,33 +12,18 @@ int same = 0;
 
 if(len > 2)
 {
-    for (int i = 1; i < line.Length - 1; i++)
+    string sortedLine = String.Concat(line.OrderBy(c => c));
+    for (int i = 0; i < sortedLine.Length - 1; i++)
     {
-        //for(int j = 0; j < parts.Length; j++)
-        //{
-        //    if (parts[j] != line[i].ToString())
-        //    {
-        //        parts[j] = line[i].ToString();
-        //    }
-        //}
-        //parts[i] = line[i];
-        //Console.Write(line[i]);
-    }
-    string[] parts = line.Split(", ");
-    for (int j = 0; j < parts.Length; j++)
-    {
-        for(int k = 0; k < parts.Length; k++)
+        if (sortedLine[i] >= 'a' && sortedLine[i] <= 'z')
         {
-            if (parts[j] == parts[k])
+            if (sortedLine[i] != sortedLine[i + 1])
             {
-                same++;
+                ans++;
             }
         }
-        if(same == 1)
-        {
-            ans++;
-        }
     }
+    Console.WriteLine(ans);
 }
 else
 {
