ecade58b3911fa90cfbc0705566d5f7336bcbb82
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-04 08:22:48 +0600

    1154A: AC

--- 1154A/Program.cs
+++ 1154A/Program.cs
@@ -1,7 +1,21 @@
-﻿/**
+﻿
+
+using System.Collections;
+/**
  * Problem Link - https://codeforces.com/problemset/problem/1154/A
  * Author - Ahnaf Shahriar
  * Status - Trying
  * Date & Time - 04 November 2022, 08:12 AM
  */
 
+string input = Console.ReadLine();
+int[] inputArray = Array.ConvertAll(input.Split(' '), int.Parse);
+Array.Sort(inputArray);
+int ab = inputArray[0];
+int ac = inputArray[1];
+int bc = inputArray[2];
+int abc = inputArray[3];
+int a = abc - bc;
+int b = abc - ac;
+int c = abc - ab;
+Console.WriteLine(a + " " + b + " " + c);
