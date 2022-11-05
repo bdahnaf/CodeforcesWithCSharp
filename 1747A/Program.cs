b575266adacd4509ccfa7c6bf5f5c7f6ad5b8120
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-05 22:57:48 +0600

    Add new problem: 1747A

---
+++ 1747A/Program.cs
@@ -0,0 +1,31 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int t = int.Parse(Console.ReadLine());
+int[] ansArray = new int[t];
+for (int j = 0; j < t; j++)
+{
+    int n = int.Parse(Console.ReadLine());
+    int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+    int sum1 = 0;
+    int sum2 = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] > 0)
+        {
+            sum1 += input[i];
+        }
+        else
+        {
+            sum2 += input[i];
+        }
+    }
+    ansArray[j] = Math.Abs(Math.Abs(sum1) - Math.Abs(sum2));
+    sum1 = 0;
+    sum2 = 0;
+}
+
+for (int i = 0; i < t; i++)
+{
+    Console.WriteLine(ansArray[i]);
+}
