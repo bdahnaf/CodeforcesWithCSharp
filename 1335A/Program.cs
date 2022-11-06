127b9f8aacec9c08cd6c51ce47a9296138b9fe3e
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-06 22:40:38 +0600

    1335A: AC

--- 1335A/Program.cs
+++ 1335A/Program.cs
@@ -1,6 +1,22 @@
 ﻿/**
  * Problem Link - https://codeforces.com/problemset/problem/1335/A
  * Author - Ahnaf Shahriar
- * Status - Trying
- * Date & Time - 06 November 2022, 10:29 PM
+ * Status - Accepted
+ * Date & Time - 06 November 2022, 10:40 PM
  */
+
+int n = int.Parse(Console.ReadLine());
+int ans = 0;
+for (int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    //for (int a = number - 1, b = 1; a > b; a--, b++)
+    //{
+    //    ans++;
+    //}
+    //Console.WriteLine(ans);
+    ans = (number - 1) / 2;
+    Console.WriteLine(ans);
+    ans = 0;
+}
+
