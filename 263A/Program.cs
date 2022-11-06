0b7d28cfe93324d79461b54032585bb8c836d183
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-06 22:07:36 +0600

    Add comment

--- 263A/Program.cs
+++ 263A/Program.cs
@@ -1,8 +1,8 @@
 ﻿/**
  * Problem Link - https://codeforces.com/problemset/problem/263/A
  * Author - Ahnaf Shahriar
- * Status - Trying
- * Date & Time - 06 November 2022, 09:55 PM
+ * Status - Accepted
+ * Date & Time - 06 November 2022, 10:07 PM
  */
 int[] input;
 int[,] arr = new int[5,5];
