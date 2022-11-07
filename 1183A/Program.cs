86b8443d23de26a331e64672f014b3f4bb4a301f
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-08 00:00:22 +0600

    1183A: AC

--- 1183A/Program.cs
+++ 1183A/Program.cs
@@ -1,7 +1,28 @@
 ﻿/**
  * Problem Link - https://codeforces.com/problemset/problem/1183/A
  * Author - Ahnaf Shahriar
- * Status - Trying
- * Date & Time - 07 November 2022, 11:44 PM
+ * Status - Accepted
+ * Date & Time - 07 November 2022, 11:59 PM
  */
+
 int number = int.Parse(Console.ReadLine());
+int digitSum = _GetSumDigit(number);
+while (digitSum % 4 != 0)
+{
+    number++;
+    digitSum = _GetSumDigit(number);
+}
+Console.WriteLine(number);
+
+int _GetSumDigit(int number)
+{
+    int digit = 0;
+    int sum = 0;
+    while (number > 0)
+    {
+        digit = number % 10;
+        number = number / 10;
+        sum = sum + digit;
+    }
+    return sum;
+}
