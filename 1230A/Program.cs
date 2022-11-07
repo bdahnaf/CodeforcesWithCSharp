bdb5975487b7356acae801e55908e0a6ebbb7eab
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-08 00:21:18 +0600

    WIP: loop

--- 1230A/Program.cs
+++ 1230A/Program.cs
@@ -4,6 +4,7 @@
  * Status - Trying
  * Date & Time - 06 November 2022, 12:09 AM
  */
+
 int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
 int sum = input[0] + input[1] + input[2] + input[3];
 if (sum % 2 == 0)
@@ -13,4 +14,14 @@ if (sum % 2 == 0)
 else
 {
     Console.WriteLine("NO");
+}
+
+for (int i = 0; i < 4; i++)
+{
+    for (int j = i + 1; j <= i; j++)
+    {
+        Console.WriteLine("Adding " + input[i] + " + " + input[j] + " = " + input[i] + input[j]);
+    }
+
+    Console.WriteLine("__------------------------");
 }
