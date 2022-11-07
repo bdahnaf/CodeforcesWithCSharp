e092989df101a855ba33a3f94ddbb9cc61e32f92
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-07 23:45:33 +0600

    Add new problem: 1183A

---
+++ 1183A/Program.cs
@@ -0,0 +1,7 @@
+﻿/**
+ * Problem Link - https://codeforces.com/problemset/problem/1183/A
+ * Author - Ahnaf Shahriar
+ * Status - Trying
+ * Date & Time - 07 November 2022, 11:44 PM
+ */
+int number = int.Parse(Console.ReadLine());
