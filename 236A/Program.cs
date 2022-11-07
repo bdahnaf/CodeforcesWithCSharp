49d75f04c472c165650cfabfdd4c5d22b8817ac0
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-07 18:42:40 +0600

    string to char in array

--- 236A/Program.cs
+++ 236A/Program.cs
@@ -4,3 +4,23 @@
  * Status - Trying
  * Date & Time - 07 November 2022, 06:36 PM
  */
+
+string word = Console.ReadLine();
+
+int len = word.Length;
+char[] wordArr = new char[len];
+int flag = 0;
+int pos = 0;
+
+for (int i = 0; i < len; i++)
+{
+    for (int j = 0; j <= i; j++)
+    {
+        //if (wordArr[j] == word[i])
+        //{
+        //    flag = 1;
+        //    pos
+        //}
+        wordArr[i] = word[i];
+    }
+}
