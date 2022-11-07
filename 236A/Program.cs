f3a44e7f27917b659d29ab8ea5026c1b1e456d6e
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-07 23:14:59 +0600

    Add comment

--- 236A/Program.cs
+++ 236A/Program.cs
@@ -1,8 +1,8 @@
 ﻿/**
  * Problem Link - https://codeforces.com/problemset/problem/236/A
  * Author - Ahnaf Shahriar
- * Status - Trying
- * Date & Time - 07 November 2022, 06:36 PM
+ * Status - Accepted
+ * Date & Time - 07 November 2022, 11:14 PM
  */
 
 string word = Console.ReadLine();
