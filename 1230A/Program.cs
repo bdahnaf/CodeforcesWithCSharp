57b7ec20288bd3d5242b3706458cbbc67408635d
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-08 00:11:54 +0600

    Add new problem: 1230A

---
+++ 1230A/Program.cs
@@ -0,0 +1,16 @@
+﻿/**
+ * Problem Link - https://codeforces.com/problemset/problem/1230/A
+ * Author - Ahnaf Shahriar
+ * Status - Trying
+ * Date & Time - 06 November 2022, 12:09 AM
+ */
+int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+int sum = input[0] + input[1] + input[2] + input[3];
+if (sum % 2 == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
