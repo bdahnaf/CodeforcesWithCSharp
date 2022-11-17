5f42b902c9aa2b35d74a8d7e09607ab69dd4dcb3
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-17 23:06:09 +0600

    581A AC

--- 581A/Program.cs
+++ 581A/Program.cs
@@ -2,8 +2,9 @@
  * Problem Link - https://codeforces.com/problemset/problem/581/A
  * Author - Ahnaf Shahriar
  * Status - Accepted
- * Date & Time - 09 November 2022, 09:13 PM
+ * Date & Time - 17 November 2022, 11:05 PM
  */
+
 int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
 int a = input[0];
 int b = input[1];
@@ -14,12 +15,44 @@ int same = 0;
 if (a > b)
 {
     dif = b;
-    b = 0;
     a = a - b;
+    b = 0;
+    
+    //Console.WriteLine(a);
+    if (a % 2 == 0)
+    {
+        same = a / 2;
+    }
+    else
+    {
+        same = (a - 1) / 2;
+    }
 }
 else if (b > a)
 {
     dif = a;
-    a = 0;
     b = b - a;
+    a = 0;
+    //Console.WriteLine("b" + b);
+    if (b % 2 == 0)
+    {
+        same = b / 2;
+    }
+    else if(b % 2 != 0 && b > 2)
+    {
+        same = (b - 1) / 2;
+    }
+    else
+    {
+        same = b - 1;
+    }
 }
+else
+{
+    dif = a;
+    same = 0;
+}
+
+//Console.WriteLine(dif);
+//Console.WriteLine(same);
+Console.WriteLine(dif + " " + same);
