c163484bffd525bbf7ee08ba94d1c494caca61b2
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-11-17 23:31:54 +0600

    Input problem

--- 1754A/Program.cs
+++ 1754A/Program.cs
@@ -4,3 +4,41 @@
  * Status - Trying
  * Date & Time - 17 November 2022, 11:17 PM
  */
+int n = int.Parse(Console.ReadLine());
+string[] ans = new string[n];
+while (n > 0)
+{
+    int j = 0;
+    int t = int.Parse(Console.ReadLine());
+    string line = Console.ReadLine();
+    int q = 0;
+    int a = 0;
+    for (int i = 0; i < line.Length; i++)
+    {
+        if (line[i] == 'Q')
+        {
+            q++;
+        }
+        else
+        {
+            a++;
+        }
+    }
+
+    if (a >= q)
+    {
+        ans[j] = "Yes";
+    }
+    else
+    {
+        ans[j] = "No";
+    }
+    j++;
+    n--;
+}
+
+
+for (int i = 0; i < ans.Length; i++)
+{
+    Console.WriteLine(ans[i]);
+}
